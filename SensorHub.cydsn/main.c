a2777afa06a929404d7f504352cf7e1b8d4cfbeb
Etienne Roberge <e.roberge.er@gmail.com>
2021-08-25 14:26:29 -0400

    Hub send test sensor data

--- SensorHub.cydsn/main.c
+++ SensorHub.cydsn/main.c
@@ -15,38 +15,7 @@
 #include <stdlib.h>
 #include "main.h"
 
-uint8 sensorValueBuffer[BUFFER_SIZE];
-
-uint32 readSensor(uint16 sensorAddress, uint8 sensorSize)
-{
-    uint32 status = TRANSFER_ERROR;
-    
-    memset(sensorValueBuffer, 0, BUFFER_SIZE);
-    (void) I2CM_I2CMasterClearStatus();
-    
-    uint32 commFlag = I2CM_I2CMasterReadBuf(sensorAddress,
-                                    sensorValueBuffer, sensorSize*2,
-                                    I2CM_I2C_MODE_COMPLETE_XFER);
-    
-    if(I2CM_I2C_MSTR_NO_ERROR ==  commFlag)
-    {
-        /* If I2C read started without errors, 
-        / wait until master complete read transfer */
-        while(0u == (I2CM_I2CMasterStatus() & I2CM_I2C_MSTAT_RD_CMPLT))
-        {
-            /* Wait */
-        }
-        
-        /* Display transfer status */
-        if(0u == (I2CM_I2C_MSTAT_ERR_XFER & I2CM_I2CMasterStatus()))
-        {
-            uint32 sizeRead = I2CM_I2CMasterGetReadBufSize();
-            status = TRANSFER_CMPLT;
-        }
-    }
-
-    return (status);
-}
+uint8 sensorValueBuffer[BUFFER_SIZE+1];
 
 int main(void)
 {
@@ -63,20 +32,24 @@ int main(void)
         sensorList[i].nbTaxels = nbTaxelList[i];
     }
     
-    
     for(;;)
     {
-        
-        /* Read response packet from the slave */
-        if (TRANSFER_CMPLT == readSensor(0x08, 25))
+        for(uint8 i=0; i<NUMBER_OF_SENSORS; ++i)
         {
-            // Send it through UART
-            comm_putmsg((uint8*)sensorValueBuffer, BUFFER_SIZE);
+            sensorValueBuffer[0] = sensorList[i].i2cAddr;
+            for(uint8 ii=0; ii<sensorList[i].nbTaxels; ++ii)
+            {
+                uint8 r = rand() % 100;
+                sensorValueBuffer[(ii)*2 + 1] = 0x05; 
+                sensorValueBuffer[(ii)*2 + 2] = r; 
+            }
+            comm_putmsg((uint8*)sensorValueBuffer, (sensorList[i].nbTaxels)*2+1);
+            // Delay (ms)
+            CyDelay(200u);
         }
-        
-        // Delay (ms)
-        CyDelay(50u);
+        CyDelay(100u);
     }
+     
 }
 
 /* [] END OF FILE */
