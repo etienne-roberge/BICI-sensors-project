7b54c50b4401bf17611fe78a390841b044709337
Abed <al.mrad.abed@gmail.com>
2021-08-30 15:28:40 -0400

    Hello

--- FingerTip.cydsn/PSoC_4_Capsense_Tuning_Project/Design01.cydsn/main.c
+++ FingerTip.cydsn/PSoC_4_Capsense_Tuning_Project/Design01.cydsn/main.c
@@ -13,33 +13,29 @@
 
 int main(void)
 {
-
+    CyGlobalIntEnable; /* Enable global interrupts. */
+    __enable_irq(); /* Enable global interrupts. */
     /* Place your initialization/startup code here (e.g. MyInst_Start()) */
 
-   CyGlobalIntEnable;
-    __enable_irq(); /* Enable global interrupts. */
     
     I2C_Start();
-    
-    I2C_EzI2CSetBuffer1(sizeof(CapSense_dsRam),
-                        sizeof(CapSense_dsRam),
-                        (uint8_t *)&CapSense_dsRam);
-    
+    I2C_EzI2CSetBuffer1(sizeof(CapSense_dsRam), sizeof(CapSense_dsRam), (uint8_t*) &CapSense_dsRam);
     CapSense_Start();
     CapSense_ScanAllWidgets();
     /* Place your initialization/startup code here (e.g. MyInst_Start()) */
-
     for(;;)
     {
-        if(!CapSense_IsBusy())
-        {
-            CapSense_ProcessAllWidgets();
-           
-            CapSense_ScanAllWidgets(); 
-            CapSense_RunTuner();
-        }
-        
+        /* Place your application code here. */
+          if(!CapSense_IsBusy())
+    {
+        CapSense_ProcessAllWidgets();
+        /*CapSense_UpdateAllBaselines(); */
+        CapSense_ScanAllWidgets(); 
+        CapSense_RunTuner();
     }
-}
+    }
+    }
+
 
 /* [] END OF FILE */
+
