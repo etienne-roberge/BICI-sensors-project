0327a32ad16815e918996a087391da829f44803d
Rachel <rthom@stanford.edu>
2021-09-27 17:53:57 -0400

    Added timer to SensorHubTest

--- SensorHubTest.cydsn/main.c
+++ SensorHubTest.cydsn/main.c
@@ -15,12 +15,32 @@
 #include <stdlib.h>
 #include "main.h"
 
-uint8 sensorValueBuffer[BUFFER_SIZE+1];
+uint32 counter = 0; // initialize counter for timestamps
+
+CY_ISR (Timer_Int_Handler)
+{
+    if (Timer_GetInterruptSource() == Timer_INTR_MASK_CC_MATCH)
+    {
+        counter += Timer_ReadCounter(); // add 16-bit timer count to our 32-bit counter
+        Timer_WriteCounter(0); // reset the counter
+        Timer_ClearInterrupt(Timer_INTR_MASK_CC_MATCH); // clear the interrupt
+    }
+
+    if (Timer_GetInterruptSource() == Timer_INTR_MASK_TC)
+    {
+       Timer_ClearInterrupt(Timer_INTR_MASK_TC);
+    }
+    
+}
 
 int main(void)
 {
     CyGlobalIntEnable;
     comm_init();
+    
+    /* Start Timer */
+    Timer_Start();
+    Timer_Int_StartEx(Timer_Int_Handler);
 
      /* Start the I2C Master */
     I2CM_Start();
@@ -36,19 +56,35 @@ int main(void)
     {
         for(uint8 i=0; i<NUMBER_OF_SENSORS; ++i)
         {
-            sensorValueBuffer[0] = sensorList[i].i2cAddr;
+            // add sensor number to data (1 byte)
+            uartBuffer[0] = sensorList[i].i2cAddr;
+            
+            // trigger counter interrupt to get timestamp 
+            Timer_SetInterrupt( Timer_INTR_MASK_CC_MATCH );
+            
+            // add timestamp to data (4 bytes)
+            uartBuffer[1] = (uint8)(counter >> 24);
+            uartBuffer[2] = (uint8)(counter >> 16);
+            uartBuffer[3] = (uint8)(counter >> 8);
+            uartBuffer[4] = (uint8)counter;
+            
+            // add sensor readings to data
             for(uint8 ii=0; ii<sensorList[i].nbTaxels; ++ii)
             {
                 uint8 r = rand() % 100;
-                sensorValueBuffer[(ii)*2 + 1] = 0x05; 
-                sensorValueBuffer[(ii)*2 + 2] = r; 
+                uartBuffer[(ii)*2 + MESSAGE_HEADER_SIZE] = 0x05; 
+                uartBuffer[(ii)*2 + MESSAGE_HEADER_SIZE + 1] = r; 
             }
-            comm_putmsg((uint8*)sensorValueBuffer, (sensorList[i].nbTaxels)*2+1);
+            comm_putmsg((uint8*)uartBuffer, (sensorList[i].nbTaxels)*2+MESSAGE_HEADER_SIZE);
             // Delay (ms)
             CyDelay(200u);
         }
         CyDelay(100u);
     }
+    
+    // stop timer and associated interrupt
+    Timer_Stop();
+    Timer_Int_Stop();
      
 }
 
