9b203e56446099d2ff6bf4d71d3db708af13e8f7
Abed <abed-al-rahman.al-mrad.1@ens.etsmtl.ca>
2021-11-12 17:31:36 -0500

    Isolated Allegro Hand

---
+++ ROS_COMM_WS/PCAN-Basic_Linux-4.5.2/pcanbasic_java/libpcanbasic_jni/src/VersionNo.h
@@ -0,0 +1,9 @@
+#define VERSION_MAJOR		4
+#define VERSION_MINOR		5
+#define VERSION_PATCH		0
+#define VERSION_BUILD		0
+
+#define FILEVER 4,5,0,0
+#define STRFILEVER "4.5.0.0"
+#define STRPRODUCTVER "4.5"
+#define COPYRIGHT "Copyright © 2021 PEAK-System Technik GmbH"
